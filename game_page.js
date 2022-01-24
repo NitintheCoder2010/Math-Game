b0643f21c6fdc4b2af99a47693afb0ee43da1140
NitintheCoder2010 <80655389+NitintheCoder2010@users.noreply.github.com>
2022-01-23 18:39:08 -0800

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -9,3 +9,21 @@ document.getElementById("player2_name").innerHTML=player_2_name;
 
 document.getElementById("player1_score").innerHTML=player_1_score;
 document.getElementById("player2_score").innerHTML=player_2_score;
+
+function send() {
+    num1 = document.getElementById("number_1").value;
+    num2 = document.getElementById("number_2").value;
+    answer = parseInt(num1) * parseInt(num2);
+
+    question_text = "<h4> " + num1 +" X "+ num2 +" </h4> <br>";
+    answer_text = "Answer : <input id ='user_answer' type = 'text'> <br>";
+    submit_text = "<button onclick = 'correct()'>Submit</button>";
+
+    output_text = question_text + answer_text +  submit_text;
+
+    document.getElementById("output").innerHTML = output_text;
+
+    document.getElementById("number_1").value = " ";
+    document.getElementById("number_2").value = " ";
+
+}
