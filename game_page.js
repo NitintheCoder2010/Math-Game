3eb5d4560ce000d47015c57d86da23d16c126d1e
NitintheCoder2010 <80655389+NitintheCoder2010@users.noreply.github.com>
2022-02-06 18:27:52 -0800

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -1,5 +1,7 @@
-player_1_score=0
-player_2_score=0
+player_1_score=0;
+player_2_score=0;
+question_turn="p1";
+answer_turn="p2";
 
 player_1_name=localStorage.getItem("player1_name");
 player_2_name=localStorage.getItem("player2_name");
@@ -26,4 +28,26 @@ function send() {
     document.getElementById("number_1").value = " ";
     document.getElementById("number_2").value = " ";
 
+
+}
+
+function correct() {
+get_answer = document.getElementById("user_answer").value;
+if(get_answer == answer){
+    if(answer_turn == "p1"){
+        player_1_score = player_1_score + 1;
+        document.getElementById("player1_score").innerHTML=player_1_score;
+    }else{
+        player_2_score = player_2_score + 1;
+        document.getElementById("player2_score").innerHTML=player_2_score;
+    }
+}
+if (question_turn == "p1"){
+    question_turn = "p2";
+    answer_turn = "p1";
+}
+else {
+    question_turn = "p1";
+    answer_turn = "p2";
+}
 }
